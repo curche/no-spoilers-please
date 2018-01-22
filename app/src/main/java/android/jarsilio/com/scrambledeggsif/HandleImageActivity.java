a996c7c1c12d1fadb975bc195bd704f1c8a07bf6
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-01-22 19:03:50 +0100

    Add empty HandleImageActivity

---
+++ app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
@@ -0,0 +1,13 @@
+package android.jarsilio.com.scrambledeggsif;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandleImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_image);
+    }
+}

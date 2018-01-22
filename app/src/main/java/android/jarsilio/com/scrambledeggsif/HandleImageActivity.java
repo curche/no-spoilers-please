5fd95b9c22b24c1975794175a63e7be0a5da82e3
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-01-22 19:06:33 +0100

    HandleImageActivity: add callback if intent is SEND_DATA while calling the view. Call a 'handleSendImage(intent)' method which does nothing for the moment.

--- app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
+++ app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
@@ -1,5 +1,6 @@
 package android.jarsilio.com.scrambledeggsif;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class HandleImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handle_image);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
+            }
+        }
+    }
+
+    private void handleSendImage(Intent intent) {
     }
 }

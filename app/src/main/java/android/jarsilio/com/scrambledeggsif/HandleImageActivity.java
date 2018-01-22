9c0c6bcb7115aafe0a4581403067d0b0a53a8ffd
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-01-22 19:47:54 +0100

    HandleImageActivity (permissions): ask for READ_EXTERNAL_STORAGE permission to be able to read the files the Intent is sharing with us.

--- app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
+++ app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
@@ -1,10 +1,15 @@
 package android.jarsilio.com.scrambledeggsif;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HandleImageActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,29 @@ public class HandleImageActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if (type.startsWith("image/")) {
-                handleSendImage(intent); // Handle single image being sent
+                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    // TODO after the check is done, the file won't be shared anymore. Tell the user s/he'll need to re-share the image
+                } else {
+                    handleSendImage(intent);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
             }
         }
     }

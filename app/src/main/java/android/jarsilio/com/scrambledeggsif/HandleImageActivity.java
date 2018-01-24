7626beb67bf2b6887630cb98ec038a5205aa7a64
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-01-24 22:06:23 +0100

    HandleImageActivity: getExifAttributes with a lazy initializer. Add all my current attributes to the Set because ExifInterface doesn't complain if the image doesn't have the specific attribute.

--- app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
+++ app/src/main/java/android/jarsilio/com/scrambledeggsif/HandleImageActivity.java
@@ -25,11 +25,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class HandleImageActivity extends AppCompatActivity {
     private static final String TAG = "HandleImageActivity";
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1000;
+
+    private static Set<String> exifAttributes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -137,30 +141,15 @@ public class HandleImageActivity extends AppCompatActivity {
     private void removeExifData(File image) {
         try {
             ExifInterface exifInterface = new ExifInterface(image.toString());
-            Field[] fields = ExifInterface.class.getDeclaredFields();
-            // Get all fields that the concrete Android-Java implementation have and delete them
-            for (Field field : fields) {
-                if (Modifier.isPublic(field.getModifiers()) &&
-                        Modifier.isStatic(field.getModifiers()) &&
-                        Modifier.isFinal(field.getModifiers())) {
-
-                    if (field.getType() == String.class) {
-                        String attribute = (String) field.get(String.class);
-                        Log.d(TAG, String.format("%s (%s) old value: %s", field.getName(), attribute, exifInterface.getAttribute(attribute)));
-                        exifInterface.setAttribute(attribute, null);
-                        Log.d(TAG, String.format("%s (%s) new value: %s", field.getName(), attribute, exifInterface.getAttribute(attribute)));
-                    }
-                }
+            for (String attribute : getExifAttributes()) {
+                exifInterface.setAttribute(attribute, null);
             }
             exifInterface.saveAttributes();
+
         } catch (IOException e) {
-            Log.e(TAG, "Error reading Exif data from image");
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            Log.e(TAG, "Error setting Exif data for new temp image");
+            Log.e(TAG, "Error while trying to read Exif from file");
             e.printStackTrace();
         }
-
     }
 
     private String getRealPathFromURI(Uri contentUri) {
@@ -172,5 +161,173 @@ public class HandleImageActivity extends AppCompatActivity {
         String result = cursor.getString(columnIndex);
         cursor.close();
         return result;
+    }
+
+    private static Set<String> getExifAttributes() {
+        if (exifAttributes == null) {
+            exifAttributes = new HashSet<String>();
+
+            // From my current Android SDK
+            exifAttributes.add("FNumber");
+            exifAttributes.add("ApertureValue");
+            exifAttributes.add("Artist");
+            exifAttributes.add("BitsPerSample");
+            exifAttributes.add("BrightnessValue");
+            exifAttributes.add("CFAPattern");
+            exifAttributes.add("ColorSpace");
+            exifAttributes.add("ComponentsConfiguration");
+            exifAttributes.add("CompressedBitsPerPixel");
+            exifAttributes.add("Compression");
+            exifAttributes.add("Contrast");
+            exifAttributes.add("Copyright");
+            exifAttributes.add("CustomRendered");
+            exifAttributes.add("DateTime");
+            exifAttributes.add("DateTimeDigitized");
+            exifAttributes.add("DateTimeOriginal");
+            exifAttributes.add("DefaultCropSize");
+            exifAttributes.add("DeviceSettingDescription");
+            exifAttributes.add("DigitalZoomRatio");
+            exifAttributes.add("DNGVersion");
+            exifAttributes.add("ExifVersion");
+            exifAttributes.add("ExposureBiasValue");
+            exifAttributes.add("ExposureIndex");
+            exifAttributes.add("ExposureMode");
+            exifAttributes.add("ExposureProgram");
+            exifAttributes.add("ExposureTime");
+            exifAttributes.add("FileSource");
+            exifAttributes.add("Flash");
+            exifAttributes.add("FlashpixVersion");
+            exifAttributes.add("FlashEnergy");
+            exifAttributes.add("FocalLength");
+            exifAttributes.add("FocalLengthIn35mmFilm");
+            exifAttributes.add("FocalPlaneResolutionUnit");
+            exifAttributes.add("FocalPlaneXResolution");
+            exifAttributes.add("FocalPlaneYResolution");
+            exifAttributes.add("FNumber");
+            exifAttributes.add("GainControl");
+            exifAttributes.add("GPSAltitude");
+            exifAttributes.add("GPSAltitudeRef");
+            exifAttributes.add("GPSAreaInformation");
+            exifAttributes.add("GPSDateStamp");
+            exifAttributes.add("GPSDestBearing");
+            exifAttributes.add("GPSDestBearingRef");
+            exifAttributes.add("GPSDestDistance");
+            exifAttributes.add("GPSDestDistanceRef");
+            exifAttributes.add("GPSDestLatitude");
+            exifAttributes.add("GPSDestLatitudeRef");
+            exifAttributes.add("GPSDestLongitude");
+            exifAttributes.add("GPSDestLongitudeRef");
+            exifAttributes.add("GPSDifferential");
+            exifAttributes.add("GPSDOP");
+            exifAttributes.add("GPSImgDirection");
+            exifAttributes.add("GPSImgDirectionRef");
+            exifAttributes.add("GPSLatitude");
+            exifAttributes.add("GPSLatitudeRef");
+            exifAttributes.add("GPSLongitude");
+            exifAttributes.add("GPSLongitudeRef");
+            exifAttributes.add("GPSMapDatum");
+            exifAttributes.add("GPSMeasureMode");
+            exifAttributes.add("GPSProcessingMethod");
+            exifAttributes.add("GPSSatellites");
+            exifAttributes.add("GPSSpeed");
+            exifAttributes.add("GPSSpeedRef");
+            exifAttributes.add("GPSStatus");
+            exifAttributes.add("GPSTimeStamp");
+            exifAttributes.add("GPSTrack");
+            exifAttributes.add("GPSTrackRef");
+            exifAttributes.add("GPSVersionID");
+            exifAttributes.add("ImageDescription");
+            exifAttributes.add("ImageLength");
+            exifAttributes.add("ImageUniqueID");
+            exifAttributes.add("ImageWidth");
+            exifAttributes.add("InteroperabilityIndex");
+            exifAttributes.add("ISOSpeedRatings");
+            exifAttributes.add("ISOSpeedRatings");
+            exifAttributes.add("JPEGInterchangeFormat");
+            exifAttributes.add("JPEGInterchangeFormatLength");
+            exifAttributes.add("LightSource");
+            exifAttributes.add("Make");
+            exifAttributes.add("MakerNote");
+            exifAttributes.add("MaxApertureValue");
+            exifAttributes.add("MeteringMode");
+            exifAttributes.add("Model");
+            exifAttributes.add("NewSubfileType");
+            exifAttributes.add("OECF");
+            exifAttributes.add("AspectFrame");
+            exifAttributes.add("PreviewImageLength");
+            exifAttributes.add("PreviewImageStart");
+            exifAttributes.add("ThumbnailImage");
+            exifAttributes.add("Orientation");
+            exifAttributes.add("PhotometricInterpretation");
+            exifAttributes.add("PixelXDimension");
+            exifAttributes.add("PixelYDimension");
+            exifAttributes.add("PlanarConfiguration");
+            exifAttributes.add("PrimaryChromaticities");
+            exifAttributes.add("ReferenceBlackWhite");
+            exifAttributes.add("RelatedSoundFile");
+            exifAttributes.add("ResolutionUnit");
+            exifAttributes.add("RowsPerStrip");
+            exifAttributes.add("ISO");
+            exifAttributes.add("JpgFromRaw");
+            exifAttributes.add("SensorBottomBorder");
+            exifAttributes.add("SensorLeftBorder");
+            exifAttributes.add("SensorRightBorder");
+            exifAttributes.add("SensorTopBorder");
+            exifAttributes.add("SamplesPerPixel");
+            exifAttributes.add("Saturation");
+            exifAttributes.add("SceneCaptureType");
+            exifAttributes.add("SceneType");
+            exifAttributes.add("SensingMethod");
+            exifAttributes.add("Sharpness");
+            exifAttributes.add("ShutterSpeedValue");
+            exifAttributes.add("Software");
+            exifAttributes.add("SpatialFrequencyResponse");
+            exifAttributes.add("SpectralSensitivity");
+            exifAttributes.add("StripByteCounts");
+            exifAttributes.add("StripOffsets");
+            exifAttributes.add("SubfileType");
+            exifAttributes.add("SubjectArea");
+            exifAttributes.add("SubjectDistance");
+            exifAttributes.add("SubjectDistanceRange");
+            exifAttributes.add("SubjectLocation");
+            exifAttributes.add("SubSecTime");
+            exifAttributes.add("SubSecTimeDigitized");
+            exifAttributes.add("SubSecTimeDigitized");
+            exifAttributes.add("SubSecTimeOriginal");
+            exifAttributes.add("SubSecTimeOriginal");
+            exifAttributes.add("ThumbnailImageLength");
+            exifAttributes.add("ThumbnailImageWidth");
+            exifAttributes.add("TransferFunction");
+            exifAttributes.add("UserComment");
+            exifAttributes.add("WhiteBalance");
+            exifAttributes.add("WhitePoint");
+            exifAttributes.add("XResolution");
+            exifAttributes.add("YCbCrCoefficients");
+            exifAttributes.add("YCbCrPositioning");
+            exifAttributes.add("YCbCrSubSampling");
+            exifAttributes.add("YResolution");
+
+            // Get all fields that the concrete Android-Java implementation have and delete them
+            Field[] fields = ExifInterface.class.getDeclaredFields();
+            for (Field field : fields) {
+                if (Modifier.isPublic(field.getModifiers()) &&
+                        Modifier.isStatic(field.getModifiers()) &&
+                        Modifier.isFinal(field.getModifiers())) {
+
+                    if (field.getType() == String.class) {
+                        String attribute;
+                        try {
+                            attribute = (String) field.get(String.class);
+                            exifAttributes.add(attribute);
+                        } catch (IllegalAccessException e) {
+                            Log.e(TAG, "Error trying to read ExifAttributes fields");
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+
+        return exifAttributes;
     }
 }

f2f89824db07f458cc5edbdee719fa6f29dc32b6
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-02-08 08:00:47 +0100

    Timber logging: Add the library but still don't actually use it. Note: add an App class that extends the almighty Application so that we can make sure that Timber has started whenever our app does something.

---
+++ app/src/main/java/com/jarsilio/android/scrambledeggsif/App.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Juan García Basilio
+ *
+ * This file is part of Scrambled Exif.
+ *
+ * Scrambled Exif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scrambled Exif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scrambled Exif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.jarsilio.android.scrambledeggsif;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}

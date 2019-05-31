697de277054842b495d9c9fe5b01862aac170d90
Juan Garcia Basilio <juanitobananas@gmail.com>
2019-05-31 07:30:49 +0200

    Add ContentProxy a.k.a (try) to add Scrambled Exif as a file chooser.
    AndroidManifest.xml: add IntentFilter for GET_CONTENT.
    If you choose the file manager (documentsui), e.g. when asked to upload
    a file to a website you can now choose Scrambled Exif as a source in
    the burger menu.
    
    ContentProxyActivity: responds to the GET_CONTENT defined in the
    AndroidManifest.xml.
    It allows to pick *one* image from the gallery and send a scrambled copy
    to the calling app using a "result" intent.
    
    I don't seem to be able to return multiple uris using clipdata
    (for >= JELLY_BEAN_MR2).
    
    It would be nice, if it would get shown in the first "open with" intent.
    
    Partially fixes #7.

---
+++ app/src/main/java/com/jarsilio/android/scrambledeggsif/ContentProxyActivity.kt
@@ -0,0 +1,63 @@
+package com.jarsilio.android.scrambledeggsif
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+import android.app.Activity
+
+const val PICK_IMAGE_REQUEST = 100
+
+/**
+ * Scrambles user-selected file from documentsui.
+ *
+ * Responds to GET_CONTENT and therefore shows Scrambled Exif
+ * in the in ellipsis menu of documentsui.
+ */
+
+class ContentProxyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_proxy)
+        openGallery()
+    }
+
+    private fun openGallery() {
+        Timber.d("Opening gallery so that the user can choose some images to share")
+
+        val intent = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                // I explicitly don't allow to select multiple images because I still haven't been able to deliver the results correctly in a result intent
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+            }
+        }
+
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.share_multiple_via)), PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri = data.data
+            Timber.d("Received image to scramble: $imageUri. Scrambling...")
+            val scrambledImageUri = ExifScrambler(this).scrambleImage(imageUri)
+            Timber.d("Sending uri in result intent: $scrambledImageUri")
+
+            // Send data back to calling app
+            val resultIntent = Intent()
+            resultIntent.data = scrambledImageUri
+            resultIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // temp permission for receiving app to read this file
+
+            setResult(RESULT_OK, resultIntent)
+        } else {
+            setResult(RESULT_CANCELED)
+        }
+
+        finish()
+    }
+}

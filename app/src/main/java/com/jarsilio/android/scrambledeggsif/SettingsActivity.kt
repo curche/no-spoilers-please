45c57e9b835d1f9f31cfa0b70035534b71c18107
Juanito <juanitobananas@gmail.com>
2019-07-21 21:45:51 +0200

    kotlinize SettingsActivity

---
+++ app/src/main/java/com/jarsilio/android/scrambledeggsif/SettingsActivity.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2018-2019 Juan García Basilio
+ *
+ * This file is part of Scrambled Exif.
+ *
+ * Scrambled Exif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scrambled Exif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scrambled Exif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.jarsilio.android.scrambledeggsif
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import android.view.MenuItem
+
+import com.jarsilio.android.scrambledeggsif.utils.Settings
+
+import timber.log.Timber
+
+/**
+ * A [PreferenceActivity] that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ *
+ *
+ * See [
+ * Android Design: Settings](http://developer.android.com/design/patterns/settings.html) for design guidelines and the [Settings
+ * API Guide](http://developer.android.com/guide/topics/ui/settings.html) for more information on developing a Settings UI.
+ */
+class SettingsActivity : AppCompatPreferenceActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    private val settings: Settings by lazy { Settings(applicationContext) }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        when (key) {
+            Settings.REWRITE_IMAGES -> Timber.d("${Settings.REWRITE_IMAGES} changed to ${settings.isRewriteImages}")
+            else -> Timber.d("Unkown preference with key: %s", key)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            super.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupActionBar()
+        addPreferencesFromResource(R.xml.settings)
+        registerPreferencesListener()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerPreferencesListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterPreferencesListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterPreferencesListener()
+    }
+
+    /**
+     * Set up the [android.app.ActionBar], if the API is available.
+     */
+    private fun setupActionBar() {
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    /**
+     * This method stops fragment injection in malicious applications.
+     * Make sure to deny any unknown fragments here.
+     */
+    override fun isValidFragment(fragmentName: String): Boolean {
+        return PreferenceFragment::class.java.name == fragmentName
+    }
+
+    private fun registerPreferencesListener() {
+        settings.preferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    private fun unregisterPreferencesListener() {
+        settings.preferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+}

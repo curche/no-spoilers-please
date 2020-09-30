58e3ee1af8a35e76caf982d8d958120c8d9f225e
Juanito <juanitobananas@gmail.com>
2020-09-30 10:25:46 +0200

    Use new ActivityResultContracts API instead of deprecated startActivityForResult.

--- app/src/main/java/com/jarsilio/android/scrambledeggsif/ContentProxyActivity.kt
+++ app/src/main/java/com/jarsilio/android/scrambledeggsif/ContentProxyActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.jarsilio.android.scrambledeggsif.utils.ExifScrambler
 import com.jarsilio.android.scrambledeggsif.utils.Utils
@@ -24,15 +25,7 @@ class ContentProxyActivity : AppCompatActivity() {
 
     private val utils: Utils by lazy { Utils(applicationContext) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_content_proxy)
-        openGallery()
-    }
-
-    private fun openGallery() {
-        Timber.d("Opening gallery so that the user can choose some images to share")
-
+    private val openGalleryIntent: Intent by lazy {
         val intent = Intent().apply {
             type = "image/*"
             action = Intent.ACTION_GET_CONTENT
@@ -41,35 +34,46 @@ class ContentProxyActivity : AppCompatActivity() {
                 putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
             }
         }
-
-        startActivityForResult(Intent.createChooser(intent, getString(R.string.share_multiple_via)), PICK_IMAGE_REQUEST)
+        Intent.createChooser(intent, getString(R.string.share_multiple_via))
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private val openGalleryActivityResultLauncher by lazy { // This can only run during onAttach or onCreate (which is the case because we first use the variable onCreate)
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null && result.data!!.data != null) {
+                val imageUri = result.data!!.data!! // I checked for null in the if condition
+                if (utils.isScrambleableImage(imageUri)) {
+                    Timber.d("Received image to scramble: $imageUri. Scrambling...")
+                    val scrambledImageUri = ExifScrambler(this).scrambleImage(imageUri)
+                    Timber.d("Sending uri in result intent: $scrambledImageUri")
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            val imageUri = data.data!! // I checked for null in the if condition
-            if (utils.isScrambleableImage(imageUri)) {
-                Timber.d("Received image to scramble: $imageUri. Scrambling...")
-                val scrambledImageUri = ExifScrambler(this).scrambleImage(imageUri)
-                Timber.d("Sending uri in result intent: $scrambledImageUri")
+                    // Send data back to calling app
+                    val resultIntent = Intent()
+                    resultIntent.data = scrambledImageUri
+                    resultIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // temp permission for receiving app to read this file
 
-                // Send data back to calling app
-                val resultIntent = Intent()
-                resultIntent.data = scrambledImageUri
-                resultIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // temp permission for receiving app to read this file
-
-                setResult(RESULT_OK, resultIntent)
+                    setResult(RESULT_OK, resultIntent)
+                } else {
+                    Timber.d("Received something that's not a jpeg or a png image ($imageUri) in a SEND_MULTIPLE. Skipping...")
+                    Toast.makeText(this, getString(R.string.image_not_scrambleable, utils.getRealFilenameFromURI(imageUri)), Toast.LENGTH_SHORT).show()
+                    setResult(RESULT_CANCELED)
+                }
             } else {
-                Timber.d("Received something that's not a jpeg or a png image ($imageUri) in a SEND_MULTIPLE. Skipping...")
-                Toast.makeText(this, getString(R.string.image_not_scrambleable, utils.getRealFilenameFromURI(imageUri)), Toast.LENGTH_SHORT).show()
                 setResult(RESULT_CANCELED)
             }
-        } else {
-            setResult(RESULT_CANCELED)
-        }
 
-        finish()
+            finish()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_content_proxy)
+
+        openGallery()
+    }
+
+    private fun openGallery() {
+        Timber.d("Opening gallery so that the user can choose some images to share")
+        openGalleryActivityResultLauncher.launch(openGalleryIntent)
     }
 }
